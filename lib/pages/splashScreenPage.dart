7b6840c58e762f340aec6514e1ab6152b495167b
lucasccustodio <contato.lucasccustodio@gmail.com>
2019-08-30 12:33:23 -0300

    Added icons, modified splash screen and more code cleanup.

--- lib/pages/splashScreenPage.dart
+++ lib/pages/splashScreenPage.dart
@@ -1,21 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:notebulk/theme.dart';
+import 'package:notebulk/widgets/util.dart';
 
 class SplashScreenPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.black,
-        extendBody: true,
-        body: SizedBox.expand(
-          child: Align(
-            alignment: Alignment.center,
-            child: Text('Notebulk',
-                style: TextStyle(
-                    fontSize: 56,
-                    fontWeight: FontWeight.w700,
-                    fontFamily: 'PalanquinDark',
-                    color: Colors.white)),
-          ),
-        ));
+    return GradientBackground(
+      appTheme: DarkTheme(),
+      child: SizedBox.expand(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/icon-adaptive.png',
+              fit: BoxFit.contain,
+              height: MediaQuery.of(context).size.height * 0.3,
+              width: MediaQuery.of(context).size.height * 0.3,
+            ),
+            Material(
+              child: Text('Notebulk',
+                  style: TextStyle(
+                      fontSize: 56,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'PalanquinDark',
+                      color: Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
